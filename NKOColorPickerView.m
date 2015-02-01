d2348a15cc3a69f5cd3cce60bba556e75f10f87a
Jose Alvarez <compositeapps>
2015-02-01 00:29:32 -0800

    removed stupid data from wrong push

--- NKOColorPickerView.m
+++ NKOColorPickerView.m
@@ -101,7 +101,6 @@ CGFloat const NKOPickerViewCrossHairshWidthAndHeight    = 38.f;
 - (void)layoutSubviews
 {
     [super layoutSubviews];
-<<<<<<< HEAD
     /*
      self.hueSatImage.frame = CGRectMake(NKOPickerViewDefaultMargin,
      NKOPickerViewDefaultMargin,
@@ -109,15 +108,6 @@ CGFloat const NKOPickerViewCrossHairshWidthAndHeight    = 38.f;
      CGRectGetHeight(self.frame) - NKOPickerViewGradientViewHeight - NKOPickerViewDefaultMargin - NKOPickerViewGradientTopMargin);
      */
     self.hueSatImage.frame = self.bounds;
-=======
-    
-    self.hueSatImage.frame = CGRectMake(NKOPickerViewDefaultMargin,
-                                        NKOPickerViewDefaultMargin,
-                                        CGRectGetWidth(self.frame) - (NKOPickerViewDefaultMargin*2),
-                                        CGRectGetHeight(self.frame) - NKOPickerViewGradientViewHeight - NKOPickerViewDefaultMargin - NKOPickerViewGradientTopMargin);
-    
-    
->>>>>>> 6b4efa87c4139a5a9bf56fa9437268fb3132f033
     [self _updateBrightnessPosition];
     [self _updateCrosshairPosition];
 }
@@ -171,11 +161,7 @@ CGFloat const NKOPickerViewCrossHairshWidthAndHeight    = 38.f;
     
     CGPoint brightnessPosition;
     brightnessPosition.x = (1.0-currentBrightness);
-<<<<<<< HEAD
     //    brightnessPosition.y = self.gradientView.center.y;//daelhoser
-=======
-//    brightnessPosition.y = self.gradientView.center.y;//daelhoser
->>>>>>> 6b4efa87c4139a5a9bf56fa9437268fb3132f033
 }
 
 - (void)_updateCrosshairPosition
@@ -250,13 +236,8 @@ CGFloat const NKOPickerViewCrossHairshWidthAndHeight    = 38.f;
 {
     if (CGRectContainsPoint(self.hueSatImage.frame,position)){
         self.crossHairs.center = position;
-<<<<<<< HEAD
         [self _updateHueSatWithMovement:position];
     }
-=======
-		[self _updateHueSatWithMovement:position];
-	}
->>>>>>> 6b4efa87c4139a5a9bf56fa9437268fb3132f033
 }
 
 #pragma mark - Lazy loading
@@ -313,15 +294,12 @@ CGFloat const NKOPickerViewCrossHairshWidthAndHeight    = 38.f;
     return self->_crossHairs;
 }
 
-<<<<<<< HEAD
 
 -(void)setColorChangeBlock:(NKOColorPickerDidChangeColorBlock)didChangeColorBlock
 {
     self->_didChangeColorBlock = didChangeColorBlock;
 }
 
-=======
->>>>>>> 6b4efa87c4139a5a9bf56fa9437268fb3132f033
 
 @end
 
